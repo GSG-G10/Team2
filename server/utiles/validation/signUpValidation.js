1ed5fa4b53aae27fa8f9b71e322d3abd35be5916
rawand1998 <rawandgaradh@gmail.com>
2021-10-03 13:06:59 +0300

    server side valiadtion

---
+++ server/utiles/validation/signUpValidation.js
@@ -0,0 +1,9 @@
+const Joi = require('joi')
+const signUpValidation = Joi.object({
+name:Joi.string().required(),
+email:Joi.string().email().required(),
+password:Joi.string().min(6).required(),
+isdmin:Joi.boolean(),
+imgUrl: Joi.any() 
+})
+module.exports=signUpValidation;
