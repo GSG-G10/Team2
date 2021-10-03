b93aee6f2f205eef96d9f9801e55cb437b2eebb2
muhammadharoun <hro19502001@gmail.com>
2021-10-03 17:26:13 +0300

    add validation

--- server/utiles/validation/logInValidation.js
+++ server/utiles/validation/logInValidation.js
@@ -1,7 +1,8 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
-const logInValidation =Joi.object({
-    email:Joi.string().email().required(),
-    password:Joi.string().min(6).required()
-})
-module.exports=logInValidation;
+const logInValidation = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = logInValidation;
--- server/utiles/validation/signUpValidation.js
+++ server/utiles/validation/signUpValidation.js
@@ -1,9 +1,11 @@
-const Joi = require('joi')
+const Joi = require('joi');
+
 const signUpValidation = Joi.object({
-name:Joi.string().required(),
-email:Joi.string().email().required(),
-password:Joi.string().min(6).required(),
-isdmin:Joi.boolean(),
-imgUrl: Joi.any() 
-})
-module.exports=signUpValidation;
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  isdmin: Joi.boolean(),
+  imgUrl: Joi.any(),
+});
+
+module.exports = signUpValidation;
