a298587dc27870a1b7f9ede548aad1edcf329962
muhammadharoun <hro19502001@gmail.com>
2021-10-05 15:02:32 +0300

    getProductslast4Query

--- server/database/queries/deleteCardQuery.js
+++
@@ -1,5 +0,0 @@
-const connection = require('../connection');
-
-const deleteCardQuery = (userId, productId) => connection.query(`delete from cart where user_id =${userId} AND product_id = ${productId}; `);
-
-module.exports = deleteCardQuery;
