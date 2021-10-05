96e502e025e5332224935a81325d2703c13ffe6b
NourAdawi <nouradwi12@gmail.com>
2021-10-05 13:41:45 +0300

    fix res.json

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
 const router = require('./routes');
 
+const loginRoute = require('./routes/login');
+
 const app = express();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(compression());
+app.use(cookieParser());
+
 app.disable('x-powered-by');
 app.use(router);
+
+app.use('/', loginRoute);
 app.get('/', (req, res) => {
   res.send('hello world');
   res.end();
