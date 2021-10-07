1ca2df330324a6e6b3f65ce7e12e1eef343aacac
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2021-10-07 12:03:30 +0300

    fix router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/footer';
 
 function App() {
-  const history = createBrowserHistory();
   return (
     <div className="container">
-      <Router history={history}>
+      <Router>
         <NavBar />
         <Footer />
       </Router>
