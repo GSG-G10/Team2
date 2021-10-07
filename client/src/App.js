9abf9e9ebcae9bda64c48d6d43325bb3ac3a0674
muhammadharoun <hro19502001@gmail.com>
2021-10-07 15:35:59 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Signup from './components/signup';
-import Login from './components/login';
+// import Signup from './components/signup';
+// import Login from './components/login';
 import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/footer';
 import ImagesSection from './Components/imagesSections';
